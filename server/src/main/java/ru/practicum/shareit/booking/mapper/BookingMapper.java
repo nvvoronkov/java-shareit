8c5b4fc814435862b7ae4177627f3a215ac2c206
Воронков Никита <nikitavoronkov@yandex.ru>
2023-11-11 19:31:58 +0300

    TZ16: fix imports

--- server/src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
+++ server/src/main/java/ru/practicum/shareit/booking/mapper/BookingMapper.java
@@ -8,9 +8,9 @@ import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserMapper;
+import ru.practicum.shareit.user.model.User;
 
 public class BookingMapper {
     public static BookingDto toBookingDto(Booking booking) {
--- server/src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ server/src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,8 +1,8 @@
 package ru.practicum.shareit.user.service;
 
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
 
 import java.util.List;
 
